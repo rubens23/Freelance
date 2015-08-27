f21a8c3350bfbd9d2eb7f8439ca79ff685004cad
Ruben Sarkisyan <rubens24@uw.edu>
2015-08-27 09:47:22 -0700

    added css

--- htdocs/php/index.php
+++
@@ -1,7 +0,0 @@
-<html>
-	<form action="login.php" method="POST">
-		Email:<input type="text" name ="email">
-		Password:<input type="password" name ="password">
-		<input type="submit">	
-	</form>
-</html>
